5746dee8940092a38bf554048110d57345af533e
Razia Munir <irazia.munir@gmail.com>
2023-06-20 13:13:21 +0500

    .

--- script.js
+++ script.js
@@ -9,14 +9,14 @@ arr.forEach(button => {
     if (e.target.innerHTML === '=') {
       string = eval(string);
       input.value = string;
-    } 
-    else if(e.target.innerHTML === 'AC'){
-        string = '';
-        input.value = string;
     }
-    else if(e.target.innerHTML === 'DEL'){
-        string = string.substring(0,string.length-1);
-        input.value = string;
+    else if (e.target.innerHTML === 'AC') {
+      string = '';
+      input.value = string;
+    }
+    else if (e.target.innerHTML === 'DEL') {
+      string = string.substring(0, string.length - 1);
+      input.value = string;
     }
     else {
       string += e.target.innerHTML;
@@ -24,7 +24,3 @@ arr.forEach(button => {
     }
   });
 });
-
-
-
-
