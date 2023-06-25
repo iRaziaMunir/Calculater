077f27c867af041c06c7dca0f0f7a65de5637a3d
Razia Munir <irazia.munir@gmail.com>
2023-06-25 12:54:10 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 
 let input = document.getElementById('inputBox');
 let buttons = document.querySelectorAll('button');
-let string = " ";
-let arr = Array.from(buttons);
+let string = "";
 
-arr.forEach(button => {
+buttons.forEach(button => {
   button.addEventListener('click', (e) => {
     if (e.target.innerHTML === '=') {
       string = eval(string);
